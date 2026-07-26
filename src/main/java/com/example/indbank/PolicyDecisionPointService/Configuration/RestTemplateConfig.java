c42c8eab95e6536c224de14402d5f3803f86c140
subhammukherjee <subhammukherjee911@gmail.com>
2026-07-27 02:07:47 +0530

    Heath ping and keepalive checks modified

--- src/main/java/com/example/indbank/PolicyDecisionPointService/Configuration/RestTemplateConfig.java
+++ src/main/java/com/example/indbank/PolicyDecisionPointService/Configuration/RestTemplateConfig.java
@@ -4,11 +4,15 @@ import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.cookie.BasicCookieStore;
 import org.apache.hc.client5.http.cookie.CookieStore;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
---
+++ src/main/java/com/example/indbank/PolicyDecisionPointService/Utility/ConnectionKeepAlive.java
@@ -0,0 +1,36 @@
+package com.example.indbank.PolicyDecisionPointService.Utility;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+public class ConnectionKeepAlive {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${app.host.authentication.service}")
+    private String authHost;
+
+    @Value("${app.url.authentication.service.health}")
+    private String healthUrl;
+
+
+    @Scheduled(fixedDelay = 30000)
+    public void keepAlive() {
+        try {
+            String url = authHost + healthUrl;
+            restTemplate.headForHeaders(url);
+            log.debug("Health ping to Authentication Service successful");
+        }
+        catch (Exception e) {
+            log.debug("Authentication Service health ping failed: {}", e.getMessage());
+        }
+    }
+}
